59e7e6f3fd60e3a1339c1b94d2cfe88be034565f
Nightfall3594 <briankristoferperez28@gmail.com>
2025-06-23 22:07:05 +0800

    Test

---
+++ main.py
@@ -0,0 +1,10 @@
+
+
+def add(n1: int, n2: int | float) -> float:
+    return n1 + n2
+
+def multiply(n1: int | float,
+             n2: int | float):
+
+    return n1 * n2
+
---
+++ test_main.py
@@ -0,0 +1,7 @@
+from main import *
+
+def test_add():
+    assert add(1,2) == 3.0
+
+def test_multiply():
+    assert multiply(9,9) == 81.0
